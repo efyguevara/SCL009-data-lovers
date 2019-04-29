dee7858e0083df9b6da4304459b56e9e49ad23e5
Stefany Guevara <efyguevara@gmail.com>
2019-04-29 16:59:56 -0400

    Agrego informacion que se debe mostrar en las tarjetas: N° de pokemon Nombre Tipo

--- src/main.js
+++ src/main.js
@@ -5,11 +5,23 @@ const pokes = POKEMON.pokemon;
 window.onload = () => { 
     //document.getElementById("root").innerHTML=``;
         for(let i = 0; i < pokes.length; i++){
-            document.getElementById("root").innerHTML += 
-            `<div id="each-card" class="card" style= "width: 18rem;">
-   <img src="${(pokes[i].img)}" class="card-img-top" alt="${(pokes[i].num)}>
-   <h5 class="card-body"> ${(pokes[i].type)}</h5>
-   </div>`
+            document.getElementById("contenedor").innerHTML += 
+            `<div class="col-sm-12 col-md-4 col-lg-2 text-center ">
+                <div id="each-card" class="card text-center">
+                    <div class="row">
+                        <div class="col-6">    
+                            <p class="col card-body text-left">N° ${(pokes[i].num)}</p>
+                        </div>
+                        <div class="form-check offset-2 col-4 ">
+                            <input class="form-check-input" type="checkbox" value="" id="checkCompare">
+                        </div>
+                        
+                    </div>        
+                    <img src="${(pokes[i].img)}" class="card-img-top" alt="${(pokes[i].num)}>
+                    <h3 class="card-body">${(pokes[i].name)}</h3>
+                    <p class="card-body">Tipo: ${(pokes[i].type)}</p>
+                </div>
+            </div>`
 
   // <div class="card" style="width: 18rem;">
   //<img src="..." class="card-img-top" alt="...">
