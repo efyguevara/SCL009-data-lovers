08310eaee0970de94e86dce2236fdab946c03226
Stefany Guevara <efyguevara@gmail.com>
2019-05-07 16:41:53 -0400

    probando calculo: agrego funcion computedStats

--- src/data.js
+++ src/data.js
@@ -10,21 +10,72 @@ const sortpokes = (filteredPokes, sortBy, sortOrder) =>  {
   let orderedPokes = filteredPokes;
   if (sortOrder == "a-z"){
       orderedPokes.sort((a, b) =>{
-          if (a[sortBy] < b[sortBy]) { return -1; }
-          if (a[sortBy] > b[sortBy]) { return 1; }
+          if (a[sortBy] < b[sortBy]){ 
+            return -1; 
+          }
+          if (a[sortBy] > b[sortBy]){
+            return 1; 
+          }
           return 0;
       })
   }
   if(sortOrder == "z-a"){
       orderedPokes.sort((a, b) => {
-          if(a[sortBy] > b[sortBy]) { return -1; }
-          if(a[sortBy] < b[sortBy]) { return 1; }
+          if(a[sortBy] > b[sortBy]) { 
+            return -1; 
+          }
+          if(a[sortBy] < b[sortBy]) { 
+            return 1; 
+          }
           return 0;
       })
   }     
   return orderedPokes;
 }
 
+
+//haciendo contador para que recorra la data y vaya sumando de acuerdo al tipo encontrado, se guarde la cuenta en la variable de cada tipo para poder mostrarla en la tabla
+const computedStats = (pokes) => {
+  pokemons = pokes;
+  let normal = "";
+  let fire = "";
+  let water = "";
+  let grass = "";
+  let electric = "";
+  let ice = "";
+  let fighting = "";
+  let poison = "";
+  let ground = "";
+  let rock = "";
+  let flying = "";
+  let psychic = "";
+  let bug = "";
+  let ghost = "";
+  let dragon = "";
+
+  for (let i = 0; i <= pokemons.length; i++) {
+    return pokemons.type[i];}
+    
+    if(pokemons[i] == normal){
+      normal = 1;
+    }else{normal = 0}
+
+    if(pokemons[i] == fire){
+      fire = 1;
+    }
+
+  let percentPokeTypes = parseInt(pokes.length /151 * 100);
+  return percentPokeTypes;
+  }
+
+ 
+
+
+
+
 window.filterType = filterType;
 window.sortpokes = sortpokes;
+window.computedStats = computedStats;
 
+
+//buscar un contador para ir sumando a cada variable
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 /* Manejo del DOM */
 const pokes = window.POKEMON.pokemon;
 const container = document.getElementById("contenedor");
+const imprcalc = document.getElementById("imprcalculo");
 let filteredPokes = pokes;
 
 
 window.onload = () => {
 
     pokes.forEach(element => {
+        imprcalc.innerHTML = "";
         container.innerHTML +=
             `<a href="#" id="cardbtn-${(element.num)}" class="col-sm-12 col-md-4 col-lg-2 text-center cards">
                 <div class="row">
@@ -28,6 +30,7 @@ window.onload = () => {
         //el target indica el elemento seleccionado cuando el selecType escuche (que se selecciono una opcion) 
         //value da el valor que tiene el elemento seleccionado
         filteredPokes = pokes.filter((element) => element.type.includes(event.target.value));
+        imprcalc.innerHTML = "";
         container.innerHTML = "";
 
         filteredPokes.forEach(element => {
@@ -51,6 +54,7 @@ window.onload = () => {
         pokeOrder.addEventListener('change', () => {
             let sortOrder = pokeOrder.value;
             let selectionOrder = window.sortpokes(filteredPokes, "name", sortOrder); 
+            imprcalc.innerHTML = "";
             container.innerHTML = "";
             
             selectionOrder.forEach(element => { 
@@ -67,4 +71,119 @@ window.onload = () => {
                     </a>`
             });
         });
+
+    const calculo = document.getElementById("estadisticas");
+
+        calculo.addEventListener('click', () => {
+            let calculado = window.computedStats(pokes);
+            container.innerHTML = "";
+                imprcalc.innerHTML  += 
+                    `<table class="table table-striped">
+                    <h1>Estadísticas por tipo de Pokemon</h1>
+                    <thead>
+                        <tr class="bg-dark">
+                        <th scope="col">#</th>
+                        <th scope="col">Tipo</th>
+                        <th scope="col">Cantidad</th>
+                        <th scope="col">%</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="table-light">
+                        <th scope="row">1</th>
+                        <td>Normal</td>
+                        <td></td>
+                        <td></td>
+                        </tr>
+                        <tr class="table-success">    
+                        <th scope="row">2</th> 
+                        <td>Fire</td>
+                        <td></td>
+                        <td></td>
+                        </tr>
+                        <tr class="table-light"> 
+                        <th scope="row">3</th>
+                        <td>Water</td>
+                        <td></td>
+                        <td></td>
+                        </tr>
+                        <tr class="table-success"> 
+                        <th scope="row">4</th>
+                        <td>Grass</td>
+                        <td></td>
+                        <td></td>
+                        </tr>
+                        <tr class="table-light"> 
+                        <th scope="row">5</th>
+                        <td>Electric</td>
+                        <td></td>
+                        <td></td>
+                        </tr>
+                        <tr class="table-success"> 
+                        <th scope="row">6</th>
+                        <td>Ice</td>
+                        <td></td>
+                        <td></td>
+                        </tr>
+                        <tr class="table-light"> 
+                        <th scope="row">7</th>
+                        <td>Fighting</td>
+                        <td></td>
+                        <td></td>
+                        </tr>
+                        <tr class="table-success"> 
+                        <th scope="row">8</th>
+                        <td>Poison</td>
+                        <td></td>
+                        <td></td>
+                        </tr>
+                        <tr class="table-light"> 
+                        <th scope="row">9</th>
+                        <td>Ground</td>
+                        <td></td>
+                        <td></td>
+                        </tr>
+                        <tr class="table-success"> 
+                        <th scope="row">10</th>
+                        <td>Rock</td>
+                        <td></td>
+                        <td></td>
+                        </tr>
+                        <tr class="table-light"> 
+                        <th scope="row">11</th>
+                        <td>Flying</td>
+                        <td></td>
+                        <td></td>
+                        </tr>
+                        <tr class="table-success"> 
+                        <th scope="row">12</th>
+                        <td>Psychic</td>
+                        <td></td>
+                        <td></td>
+                        </tr>
+                        <tr class="table-light"> 
+                        <th scope="row">13</th>
+                        <td>Bug</td>
+                        <td></td>
+                        <td></td>
+                        </tr>
+                        <tr class="table-success"> 
+                        <th scope="row">14</th>
+                        <td>Ghost</td>
+                        <td></td>
+                        <td></td>
+                        </tr>
+                        <tr class="table-light"> 
+                        <th scope="row">15</th>
+                        <td>Dragon</td>
+                        <td></td>
+                        <td></td>
+                        </tr>                        
+                    </tbody>
+                    </table>
+                
+                    <h1>El ${calculado}% de los pokemons corresponde a este tipo.</h1>`
+                
+                console.log(calculado);        
+    })
 }
